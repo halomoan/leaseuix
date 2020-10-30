bec399097a2e5c7625fe545de6befc57f08dc8b9
Halomoan <halomoan@gmail.com>
2020-10-30 08:19:55 +0800

    list condition

--- webapp/components/gridcondition/gridcondition.controller.js
+++ webapp/components/gridcondition/gridcondition.controller.js
@@ -12,20 +12,24 @@ sap.ui.define([
 		initData: function () {
 
 			this.byId("grid1").setModel(new JSONModel([
-				{ id: "L101", title: "Base Rent 4x2", rows: 4, columns: 2 },
-				{ id: "L102", title: "Service Charge 2x3", rows: 2, columns: 3 },
+				{ id: "L101", title: "Base Rent 4x2", subtitle:'Condition: L101', rows: 4, columns: 2 },
+				{ id: "L102", title: "Service Charge 2x3", subtitle:'Condition: L102', rows: 2, columns: 3 },
 			
 			]));
 		},
 		
 		addCard: function(){
 			var oData = this.byId("grid1").getModel().getData();
-			var oCondition = { id: "L103", title: "A&P 2x2", rows: 2, columns: 2 };
+			var oCondition = { id: "L103", title: "A&P Charge 2x2", subtitle:'Condition: L103', rows: 2, columns: 2 };
 			
 			if (!this._isCardExist(oCondition.id)) {
 				oData.push(	oCondition);
 				this.byId("grid1").getModel().setProperty("/",oData);
 			}
+		},
+		findID: function(){
+			var oView = this.getView();
+			console.log(oView.byId("grid1"));	
 		},
 		
 		_isCardExist: function(sKey) {
