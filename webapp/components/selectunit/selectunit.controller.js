761485dc005ec4ae3fea654b6e2fe5c29d8bd9d0
Halomoan <halomoan@gmail.com>
2020-11-16 16:25:03 +0800

    home commit

--- webapp/components/selectunit/selectunit.controller.js
+++ webapp/components/selectunit/selectunit.controller.js
@@ -8,31 +8,44 @@ sap.ui.define([
 	'sap/m/SearchField',
 	'sap/m/Token',
 	'sap/ui/model/Filter',
-	'sap/ui/model/FilterOperator'
-], function (compLibrary, Controller, JSONModel, typeString, ColumnListItem, Label, SearchField, Token, Filter, FilterOperator) {
+	'sap/ui/model/FilterOperator',
+	"refx/leaseuix/model/formatter"
+], function(compLibrary, Controller, JSONModel, typeString, ColumnListItem, Label, SearchField, Token, Filter, FilterOperator,formatter) {
 	"use strict";
 
 	return Controller.extend("refx.leaseuix.components.selectunit.selectunit", {
-		onInit: function () {
+		formatter: formatter,
+		onInit: function() {
 			this._oMultiInput = this.getView().byId("rentalUnits");
 			this._oMultiInput.addValidator(this._onMultiInputValidate);
 			//this._oMultiInput.setTokens(this._getDefaultTokens());
 			this.initData();
-		
+
 		},
-		
-		initData: function(){
+
+		initData: function() {
+			this.oFilter1 = null;
+			this.aFilters = [];
 			var viewData = {
-				"UnitsName": "-None-"
+				"UnitsName": "-None-",
+				"TotalSize": 0,
+				"Filter": {
+					"AvailDate": null,
+					"AvailNo": {
+						value: 2,
+						min: 0,
+						max: 54
+					},
+					"AvailUnit": "0"
+				}
 			};
-			this.getView().setModel(new JSONModel(viewData));
-			
+			this.getView().setModel(new JSONModel(viewData),"viewData");
+
 			this.oColModel = new JSONModel(sap.ui.require.toUrl("refx/leaseuix/components/selectunit") + "/columns.json");
 			this.oRentalUnitsModel = new JSONModel(sap.ui.require.toUrl("refx/leaseuix/mockdata") + "/rentalunitvalues.json");
-			
+
 			this.getView().setModel(this.oRentalUnitsModel);
-			
-			
+
 		},
 
 		// #region
@@ -58,7 +71,7 @@ sap.ui.define([
 			oFilterBar.setFilterBarExpanded(false);
 			oFilterBar.setBasicSearch(this._oBasicSearchField);
 
-			this._oValueHelpDialog.getTableAsync().then(function (oTable) {
+			this._oValueHelpDialog.getTableAsync().then(function(oTable) {
 				oTable.setModel(this.oRentalUnitsModel);
 				oTable.setModel(this.oColModel, "columns");
 
@@ -67,10 +80,12 @@ sap.ui.define([
 				}
 
 				if (oTable.bindItems) {
-					oTable.bindAggregation("items", "/RentalUnits", function () {
+					oTable.bindAggregation("items", "/RentalUnits", function() {
 						return new ColumnListItem({
-							cells: aCols.map(function (column) {
-								return new Label({ text: "{" + column.template + "}" });
+							cells: aCols.map(function(column) {
+								return new Label({
+									text: "{" + column.template + "}"
+								});
 							})
 						});
 					});
@@ -83,135 +98,220 @@ sap.ui.define([
 			this._oValueHelpDialog.open();
 		},
 
-		onValueHelpOkPress: function (oEvent) {
+		onValueHelpOkPress: function(oEvent) {
 			var aTokens = oEvent.getParameter("tokens");
 			this._oMultiInput.setTokens(aTokens);
-			
+
 			this._UpdateDependantControls(aTokens);
-			
+
 			this._oValueHelpDialog.close();
 		},
-		
+
 		onTokenUpdate: function(oEvent) {
-			var aRemovedTokens =  oEvent.getParameters().removedTokens;
+			var aRemovedTokens = oEvent.getParameters().removedTokens;
 			var aTokens = oEvent.getSource().getTokens();
-			
-			for (var i = 0; i<aTokens.length;i++){
-				if( aRemovedTokens.includes(aTokens[i]) ){
-					aTokens.splice(i,1);		
+
+			for (var i = 0; i < aTokens.length; i++) {
+				if (aRemovedTokens.includes(aTokens[i])) {
+					aTokens.splice(i, 1);
 				}
 			}
-			
+
 			this._UpdateDependantControls(aTokens);
-			
+
 		},
-		
-		onClearTokens: function(oEvent){
+
+		onClearTokens: function(oEvent) {
 			this._oMultiInput.removeAllTokens();
 			this._oMultiInput.fireTokenUpdate();
 		},
 		
-		_UpdateDependantControls: function(aTokens){
-			var aFilters = [];
+		onAvailChange: function(oEvent) {
+			var oDate = new Date();
+			var oModel = this.getView().getModel("viewData");
+
+			var num = oModel.getProperty("/Filter/AvailNo/value");
+			var unit = oModel.getProperty("/Filter/AvailUnit");
+
+			switch (unit) {
+				case "0":
+					oDate.setDate(oDate.getDate() + num * 7);
+					break;
+				case "1":
+					oDate.setMonth(oDate.getMonth() + num);
+					break;
+				case "2":
+					oDate.setYear(oDate.getFullYear() + num);
+					break;
+			}
+
+			oModel.setProperty("/Filter/AvailDate", formatter.yyyyMMdd(oDate));
+
+		},
+			onResetAvail: function() {
+			var oModel = this.getView().getModel("viewData");
+			oModel.setProperty("/Filter/AvailDate", null);
+			oModel.setProperty("/Filter/AvailNo/value", 0);
+			oModel.setProperty("/Filter/AvailUnit", "0");
+			
+
+			this._setFilterAvail(true);
+		},
+
+		onApplyAvail: function() {
+			this._setFilterAvail(false);
+		},
+
+		_setFilterAvail: function(bReset) {
+
+			if (bReset) {
+				var oUnitGridBinding = sap.ui.getCore().byId("__xmlview1--unitGrid").getBinding("items");
+				if (oUnitGridBinding) {
+					oUnitGridBinding.filter(this.aFilters);	
+				}
+			} else {
+				var oModel = this.getView().getModel("viewData");
+				var sKeyDate = oModel.getProperty("/Filter/AvailDate");
+				
+				var oFilter1 = new sap.ui.model.Filter("EndDate", sap.ui.model.FilterOperator.LT, sKeyDate);
+				
+				var oUnitGridBinding = sap.ui.getCore().byId("__xmlview1--unitGrid").getBinding("items");
+				if (oUnitGridBinding) {
+					
+					var aFilters = [];
+					
+					for(var i = 0; i < this.aFilters; i++){
+						aFilters.push(this.aFilters[i]);
+					}
+					aFilters.push(oFilter1);
+					
+					oUnitGridBinding.filter(aFilters);	
+				}
+				
+			}
+		},
+
+
+		_UpdateDependantControls: function(aTokens) {
+			//var aFilters = [];
 			var sUnitsName = "";
 			var oData = this.oRentalUnitsModel.getData();
 			var iTotalSize = 0;
 			
-			for(var i = 0; i< aTokens.length; i++){
+			this.aFilters = [];
+
+			for (var i = 0; i < aTokens.length; i++) {
 				var sKey = aTokens[i].getKey();
-					if (i < aTokens.length - 1) {
-						sUnitsName += sKey + " / ";
-					} else {
-						sUnitsName += sKey ;
-					}
-				aFilters.push(
-					new Filter({ path: "UnitId", operator: FilterOperator.EQ, value1: sKey }));
-				var oUnit = oData.RentalUnits.find(unit => unit.UnitId === sKey);		
-				iTotalSize += oUnit.Size;	
-					
+				if (i < aTokens.length - 1) {
+					sUnitsName += sKey + " / ";
+				} else {
+					sUnitsName += sKey;
+				}
+				this.aFilters.push(
+					new Filter({
+						path: "UnitId",
+						operator: FilterOperator.EQ,
+						value1: sKey
+					}));
+				var oUnit = oData.RentalUnits.find(unit => unit.UnitId === sKey);
+				iTotalSize += oUnit.Size;
+
 			}
-			
+
 			if (i > 0) {
-				this.getView().getModel().setProperty("/UnitsName",sUnitsName);
-				this.getView().getModel().setProperty("/TotalSize",iTotalSize);
+				this.getView().getModel("viewData").setProperty("/UnitsName", sUnitsName);
+				this.getView().getModel("viewData").setProperty("/TotalSize", iTotalSize);
 			} else {
-				this.getView().getModel().setProperty("/UnitsName","-None-");
-				this.getView().getModel().setProperty("/TotalSize",0);
+				this.getView().getModel("viewData").setProperty("/UnitsName", "-None-");
+				this.getView().getModel("viewData").setProperty("/TotalSize", 0);
 			}
-			
+
 			var oUnitGridBinding = sap.ui.getCore().byId("__xmlview1--unitGrid").getBinding("items");
-			
-			if (oUnitGridBinding){
-			
-				oUnitGridBinding.filter(aFilters);
+
+			if (oUnitGridBinding) {
+
+				oUnitGridBinding.filter(this.aFilters);
 			}
-			
+
 		},
 
-		onValueHelpCancelPress: function () {
+		onValueHelpCancelPress: function() {
 			this._oValueHelpDialog.close();
 		},
 
-		onValueHelpAfterClose: function () {
+		onValueHelpAfterClose: function() {
 			this._oValueHelpDialog.destroy();
 		},
 
-		onFilterBarSearch: function (oEvent) {
-			
+		onFilterBarSearch: function(oEvent) {
+
 			var sSearchQuery = this._oBasicSearchField.getValue(),
 				aSelectionSet = oEvent.getParameter("selectionSet");
-			var aFilters = aSelectionSet.reduce(function (aResult, oControl) {
-				
+			var aFilters = aSelectionSet.reduce(function(aResult, oControl) {
+
 				var sType = oControl.getMetadata().getName();
-				switch(sType){
+				switch (sType) {
 					case "sap.m.Switch":
-							if (oControl.getState()) {
-								aResult.push(new Filter({
-									path: oControl.getName(),
-									operator: FilterOperator.EQ,
-									value1: oControl.getState()
-								}));
-							}
-							
-							break;
-					case "sap.m.Input" :
-							if (oControl.getValue()) {
-								aResult.push(new Filter({
-									path: oControl.getName(),
-									operator: FilterOperator.Contains,
-									value1: oControl.getValue()
-								}));
-							}
-							
-							break;
-					
-					case "sap.m.CheckBox" :
-							if (oControl.getSelected()) {
-								aResult.push(new Filter({
-									path: oControl.getName(),
-									operator: FilterOperator.EQ,
-									value1: true
-								}));
-							}
-							
-							break;
+						if (oControl.getState()) {
+							aResult.push(new Filter({
+								path: oControl.getName(),
+								operator: FilterOperator.EQ,
+								value1: oControl.getState()
+							}));
+						}
+
+						break;
+					case "sap.m.Input":
+						if (oControl.getValue()) {
+							aResult.push(new Filter({
+								path: oControl.getName(),
+								operator: FilterOperator.Contains,
+								value1: oControl.getValue()
+							}));
+						}
+
+						break;
+
+					case "sap.m.CheckBox":
+						if (oControl.getSelected()) {
+							aResult.push(new Filter({
+								path: oControl.getName(),
+								operator: FilterOperator.EQ,
+								value1: true
+							}));
+						}
+
+						break;
 				}
 				return aResult;
 			}, []);
-			
-		
-			
+
 			aFilters.push(new Filter({
 				filters: [
-					new Filter({ path: "UnitId", operator: FilterOperator.Contains, value1: sSearchQuery }),
-					new Filter({ path: "Name", operator: FilterOperator.Contains, value1: sSearchQuery }),
-					new Filter({ path: "MainCategory", operator: FilterOperator.Contains, value1: sSearchQuery }),
-					new Filter({ path: "Category", operator: FilterOperator.Contains, value1: sSearchQuery })
-					
+					new Filter({
+						path: "UnitId",
+						operator: FilterOperator.Contains,
+						value1: sSearchQuery
+					}),
+					new Filter({
+						path: "Name",
+						operator: FilterOperator.Contains,
+						value1: sSearchQuery
+					}),
+					new Filter({
+						path: "MainCategory",
+						operator: FilterOperator.Contains,
+						value1: sSearchQuery
+					}),
+					new Filter({
+						path: "Category",
+						operator: FilterOperator.Contains,
+						value1: sSearchQuery
+					})
+
 				],
 				and: false
 			}));
-		
 
 			this._filterTable(new Filter({
 				filters: aFilters,
@@ -219,10 +319,10 @@ sap.ui.define([
 			}));
 		},
 
-		_filterTable: function (oFilter) {
+		_filterTable: function(oFilter) {
 			var oValueHelpDialog = this._oValueHelpDialog;
 
-			oValueHelpDialog.getTableAsync().then(function (oTable) {
+			oValueHelpDialog.getTableAsync().then(function(oTable) {
 				if (oTable.bindRows) {
 					oTable.getBinding("rows").filter(oFilter);
 				}
@@ -237,7 +337,7 @@ sap.ui.define([
 
 		// #endregion
 
-		_getDefaultTokens: function () {
+		_getDefaultTokens: function() {
 			var ValueHelpRangeOperation = compLibrary.valuehelpdialog.ValueHelpRangeOperation;
 			var oToken1 = new Token({
 				key: "HT-1001",
--- webapp/controller/ManageUnits.controller.js
+++ webapp/controller/ManageUnits.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"refx/leaseuix/controller/BaseController",
 	'sap/ui/model/json/JSONModel',
 	"refx/leaseuix/model/formatter"
-], function(BaseController,JSONModel,formatter) {
+], function(BaseController, JSONModel, formatter) {
 	"use strict";
 
 	return BaseController.extend("refx.leaseuix.controller.ManageUnits", {
@@ -12,31 +12,15 @@ sap.ui.define([
 		},
 
 		initData: function() {
-			this.oViewData = {
-				"Filter" : {
-					"AvailDate" : null,
-					"AvailWeek" : {
-						 value: 5, min:0, max:54
-					},
-					"AvailYear" : {
-						 value: 0, min:0, max:24
-					}
-				}
-			}
-			this.oRentalUnitsModel =  new JSONModel(sap.ui.require.toUrl("refx/leaseuix/mockdata") + "/rentalunitvalues.json");
-			this.getView().setModel(this.oRentalUnitsModel);
-			this.getView().setModel(new JSONModel(this.oViewData),"viewData");
-		},
-		onAvailWeekChange: function(oEvent){
-			var oDate = new Date();
-			var numWeeks = oEvent.getParameter("value");
-			oDate.setDate(oDate.getDate() + numWeeks * 7);
-			this.oViewData.Filter.AvailDate = formatter.yyyyMMdd(oDate);
+			var oViewData = {
 			
+			};
+			this.oRentalUnitsModel = new JSONModel(sap.ui.require.toUrl("refx/leaseuix/mockdata") + "/rentalunitvalues.json");
+			this.getView().setModel(this.oRentalUnitsModel);
+			this.getView().setModel(new JSONModel(oViewData), "viewData");
 		},
-		onAvailYearChange: function(oEvent){
-			console.log(oEvent.getParameter("value"));
-		},
+	
+	
 		onExit: function() {
 
 		}
